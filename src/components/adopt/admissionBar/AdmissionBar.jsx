431c27455022f9d5e126f720b361554260851a5e
hyojinLee-git <lily960310@naver.com>
2022-02-06 17:25:38 +0900

    feat: 승인, 반려 버튼, 페이지 이동버튼

--- src/components/adopt/admissionBar/AdmissionBar.jsx
+++ src/components/adopt/admissionBar/AdmissionBar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios'
 import {MdRefresh,MdOutlineChevronLeft,MdOutlineChevronRight,MdArrowDropDown} from 'react-icons/md'
 import { checkboxState } from '../../../state/checkboxState';
@@ -10,6 +10,8 @@ import { checkedListState } from '../../../state/checkedListState';
 import { tokenState } from '../../../state/tokenState';
 import { modalState } from '../../../state/modalState';
 import { admissionState } from '../../../state/admissionState';
+import { onSubmitApproval,onSubmitRejection } from '../../../utils/submitFunction';
+import { currentPageState } from '../../../state/currentPageState';
 
 const AdmissionBar = ({onToggleDropDown}) => {
     const [checked,setChecked]=useRecoilState(checkboxState)
@@ -18,111 +20,25 @@ const AdmissionBar = ({onToggleDropDown}) => {
     const [token,]=useRecoilState(tokenState)
     const [,setShowModal]=useRecoilState(modalState)
     const [,setAdmission]=useRecoilState(admissionState)
+    const [currentPage,setCurrentPage]=useRecoilState(currentPageState)
 
 
     const dbURL=process.env.REACT_APP_DATABASE_URL;
     //이거 컴포넌트 마운트언마운트 시간대 바뀌는지 알아야할듯
-    const today=new Date();
 
     
     const onChange=()=>{
         setChecked(!checked)
     }
-    
-    
-
-    //승인버튼
-    //promise.all 쓰면 더 빠름
-    const onSubmitApproval=()=>{
-
-        //선택한 데이터 없을때
-        if(checkedList.length===0) {
-            alert('선택한 신청이 없습니다.')
-            return
-        }
-        //승인or 반려 테이블의 데이터 있으면 return
-        for(let i=0;i<checkedList.length;i++){
-            if(checkedList[i].field==='Trees_taken'||checkedList[i].field==='Rejections'){
-                alert('이미 처리된 요청이 있습니다.')
-                setCheckedList([])
-                return
-            }
-        }
-        
-        //백엔드 통신
-        checkedList.forEach(checkedListItem=>{
-
-            //data 전처리
-            const postData={
-                ...checkedListItem,
-                xp:0,
-                start_date:`${today.getFullYear()}-${today.getMonth()+1>=10? today.getMonth()+1:`0${today.getMonth()+1}`}-${today.getDate()}`,
-                tree_name:checkedListItem.tree_id
-            }
-            delete postData.field
-            delete postData.key
-            delete postData.date
-
-            //Trees_taken에 데이터 추가
-            axios.post(`${dbURL}/Trees_taken.json?auth=${token}`,postData)
-            .then(res=>console.log(res))
-            .catch(e=>console.log(e))
-
-            //Candidates에서 데이터 삭제
-            axios.delete(`${dbURL}/Candidates/${checkedListItem.key}.json?auth=${token}`)
-            .then(res=>console.log(res))
-            .catch(e=>console.log(e))
-
-            
-        })
-
-        //modal창 열기
-        setAdmission('승인')
-        setShowModal(true)
-        
-
+    const onPrevPage=()=>{
+        if(currentPage<=1) return
+        setCurrentPage(currentPage-1)
     }
-    //반려버튼
-    const onSubmitRejection=()=>{
-
-        //선택한 데이터 없을때
-        if(checkedList.length===0) {
-            alert('선택한 신청이 없습니다.')
-            return
-        }
-        //반려 테이블에 데이터 있는지 검사
-        for(let i=0;i<checkedList.length;i++){
-            if(checkedList[i].field==='Rejections'){
-                alert('이미 반려된 요청이 있습니다.')
-                setCheckedList([])
-                return
-            }
-        }
-
-        //백엔드 통신
-        checkedList.forEach(checkedListItem=>{
-
-            //Rejection에 데이터 추가
-            axios.post(`${dbURL}/Rejections.json?auth=${token}`,checkedListItem)
-            .then(res=>console.log(res))
-            .catch(e=>console.log(e))
-
-            //Candidates에서 데이터 삭제
-            axios.delete(`${dbURL}/Candidates/${checkedListItem.key}.json?auth=${token}`)
-            .then(res=>console.log(res))
-            .catch(e=>console.log(e))
-
-            //Trees_taken에서 데이터 삭제
-            axios.delete(`${dbURL}/Trees_taken/${checkedListItem.key}.json?auth=${token}`)
-            .then(res=>console.log(res))
-            .catch(e=>console.log(e))
-            
-        })
-
-        //modal창 열기
-        setShowModal(true)
-        setAdmission('반려')
-
+    const onNextPage=()=>{
+        const lastPage=Math.ceil(dataList.length/10)
+        //마지막 페이지면 return
+        if(currentPage>=lastPage) return
+        setCurrentPage(currentPage+1)
     }
     
     const onSubmitWait=()=>{
@@ -131,7 +47,7 @@ const AdmissionBar = ({onToggleDropDown}) => {
             alert('선택한 신청이 없습니다.')
             return
         }
-        //반려 테이블에 데이터 있는지 검사
+        //대기 테이블에 데이터 있는지 검사
         for(let i=0;i<checkedList.length;i++){
             if(checkedList[i].field==='Candidates'){
                 alert('대기중인 요청이 있습니다.')
@@ -149,7 +65,7 @@ const AdmissionBar = ({onToggleDropDown}) => {
             .then(res=>console.log(res))
             .catch(e=>console.log(e))
 
-            //Candidates에서 데이터 삭제
+            //Rejections에서 데이터 삭제
             axios.delete(`${dbURL}/Rejections/${checkedListItem.key}.json?auth=${token}`)
             .then(res=>console.log(res))
             .catch(e=>console.log(e))
@@ -163,11 +79,12 @@ const AdmissionBar = ({onToggleDropDown}) => {
 
         //modal창 열기
         setShowModal(true)
-        setAdmission('반려')
+        setAdmission('대기')
     }
 
     //새로고침 버튼
     const onRefresh=()=>{
+        //내생각엔 이건 그냥 현재 데이터 새로고침인데
         window.location.reload()
     }
 
@@ -201,19 +118,45 @@ const AdmissionBar = ({onToggleDropDown}) => {
                 </AdmissionBarButton>
             </div>
             <div>
-                {/* 제출부분인가 */}
-                <ProcessButton onClick={onSubmitApproval}  type='button' className={"admission-btn"} color="#6AD39F">승인</ProcessButton>
-                <ProcessButton onClick={onSubmitRejection}  type='button' className={"admission-btn"} color="#FF7E6B">반려</ProcessButton>
-                <ProcessButton type='button' className={"admission-btn"} color="#DADADA">대기</ProcessButton>
+                {/* submit button */}
+                <ProcessButton 
+                    onClick={()=>onSubmitApproval(  
+                        checkedList,
+                        setCheckedList,
+                        token,
+                        setAdmission,
+                        setShowModal)}  
+                        type='button' 
+                        className="admission-btn" 
+                        color="#6AD39F">
+                            승인
+                </ProcessButton>
+                <ProcessButton 
+                    onClick={()=>onSubmitRejection(  checkedList,
+                        setCheckedList,
+                        token,
+                        setShowModal,
+                        setAdmission,)}  
+                    type='button' 
+                    className="admission-btn" 
+                    color="#FF7E6B">
+                        반려
+                </ProcessButton>
+                <ProcessButton 
+                    type='button' 
+                    className="admission-btn" 
+                    color="#DADADA">
+                        대기
+                </ProcessButton>
             </div>
             <div>
                 <AdmissionBarButton type='button' onClick={onRefresh}>
                     <MdRefresh size={24} color='rgba(0, 0, 0, 0.54)'/>
                 </AdmissionBarButton>
-                <AdmissionBarButton type='button'>
+                <AdmissionBarButton type='button' onClick={onPrevPage}>
                     <MdOutlineChevronLeft size={24} color='rgba(0, 0, 0, 0.54)'/>
                 </AdmissionBarButton>
-                <AdmissionBarButton type='button'>
+                <AdmissionBarButton type='button' onClick={onNextPage}>
                     <MdOutlineChevronRight size={24} color='rgba(0, 0, 0, 0.54)'/>
                 </AdmissionBarButton>
             </div>
